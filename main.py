09e991aa66f96a8812bc0bb620b0a7edd1772608
azzireluto <bruno.a@outlook.com.br>
2022-07-07 17:15:30 -0300

    adding schemas and jwt support for the application

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+from datetime import datetime, timedelta
+from typing import Optional
+
 import databases
 import enum
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import jwt
 import sqlalchemy
-
-from fastapi import FastAPI
+from pydantic import BaseModel, validator
+from fastapi import FastAPI, HTTPException, Depends
 from decouple import config
+from email_validator import validate_email as validate_e, EmailNotValidError
+from passlib.context import  CryptContext
+from sqlalchemy.sql.functions import user
+from starlette.requests import Request
 
-
-DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:5433/clothes"
+DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost/{config('DB_SCHEMA')}"
 
 database = databases.Database(DATABASE_URL)
 
 metadata = sqlalchemy.MetaData()
+
+
+class UserRole(enum.Enum):
+    super_admin = "super Admin"
+    admin = "admin"
+    user = "user"
+
 
 users = sqlalchemy.Table(
     "users",
@@ -28,6 +43,7 @@ users = sqlalchemy.Table(
         server_default=sqlalchemy.func.now(),
         onupdate=sqlalchemy.func.now(),
     ),
+    sqlalchemy.Column("role", sqlalchemy.Enum(UserRole), nullable=False, server_default=UserRole.user.name )
 )
 
 
@@ -66,7 +82,73 @@ clothes = sqlalchemy.Table(
 )
 
 
+class EmailField(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v) -> str:
+        try:
+            validate_e(v)
+            return v
+        except EmailNotValidError:
+            raise ValueError("Email is not Valid")
+
+
+class BaseUser(BaseModel):
+    email: EmailField
+    full_name: str | None = None
+
+
+    @validator('full_name')
+    def validate_full_name(cls, v):
+        try:
+            first_name, last_name = v.split()
+            return v
+        except Exception:
+            raise ValueError("Your should provide a least two names")
+
+
+class UserSignIn(BaseUser):
+    password: str
+
+
+class UserSingOut(BaseUser):
+    phone:  str | None = None
+    created_at: datetime
+    last_modified_at: datetime
+
+
 app = FastAPI()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated='auto')
+
+
+def create_acess_token(user):
+    try:
+        payload = {"sub": user.id, "exp": datetime.utcnow() + timedelta(minutes=60)}
+        return jwt.encode(payload, config("JWT_SECRET"), algorithm="HS256")
+    except Exception as ex:
+        raise ex
+
+
+class CustomHTTPBearer(HTTPBearer):
+    async def __call__(self, request: Request) -> Optional[HTTPAuthorizationCredentials]:
+        res = await super().__call__(request)
+
+        try:
+            payload = jwt.decode(res.credentials, config("JWT_SECRET"), algorithms=["HS256"])
+            user = await database.fetch_one(users.select().where(users.c.id == payload['sub']))
+            request.state.user = user
+            return payload
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(401, "token is Expired")
+        except jwt.InvalidTokenError:
+            raise HTTPException(401, "invalid Token")
+
+
+oauth2_scheme = CustomHTTPBearer()
 
 
 @app.on_event("startup")
@@ -78,3 +160,18 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
+
+@app.get('/clothes', dependencies=[Depends(oauth2_scheme)])
+async def all_clothes(request: Request):
+    user = request.state.user
+    return await database.fetch_all(clothes.select())
+
+
+@app.post("/register")
+async def create_user(user: UserSignIn):
+    user.password = pwd_context.hash(user.password)
+    q = users.insert().values(**user.dict())
+    id_ = await database.execute(q)
+    created_user = await database.fetch_one(users.select().where(users.c.id == id_))
+    token = create_acess_token(created_user)
+    return {"token": token}
